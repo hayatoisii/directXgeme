cb01a33c4958ce6ad63508f13975c0590ae5d409
隼 石井 <k023g0022@g.neec.ac.jp>
2024-04-16 11:15:32 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+#pragma warning(push)
+//C4023の警告を見なかったことにする
+#pragma warning(disable:4023)
+#pragma warning(pop)
+
+// Windowsアプリでのエントリーポイント（main関数）
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	//出力ウィンドウへの文字出力
+	OutputDebugStringA("Hello,DirectX!\n");
+
+	return 0;
+}
